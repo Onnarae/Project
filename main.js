3b74cabbd0a11d0f6f454ffd7df16b7b6cd5502d
Onnarae <66655413+Onnarae@users.noreply.github.com>
2020-11-10 20:45:06 +0900

    Demo

---
+++ main.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark')
+    } else{
+        navbar.classList.remove('navbar--dark')
+    }
+});
+
+// Handle scrolling when tapping on he navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {  
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    navbarMenu.classList.remove('open');
+    scrollIntoView(link);
+});
+
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', ()=> {
+    navbarMenu.classList.toggle('open');
+});
+
+
+
+// Projects
+const productBtnContainer = document.querySelector('.product__categories');
+const productContainer = document.querySelector('.product__projects')
+const products = document.querySelectorAll('.product')
+productBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+
+    // Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+    
+    productContainer.classList.add('anim-out');
+    setTimeout(() => {
+        console.log(filter);
+        products.forEach((product) => {
+        console.log(product.dataset.type)
+        if(filter === '*' || filter === product.dataset.type){
+            product.classList.remove('invisible');
+        }else {
+            product.classList.add('invisible')
+        }
+    });
+        productContainer.classList.remove('anim-out');
+    }, 300)
+    
+});
+
